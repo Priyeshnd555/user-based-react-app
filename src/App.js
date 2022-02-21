3eda63ad04aa8800e8ae0674ce9ace046ed2d708
Priyeshnd555 <priyeshnd555@gmail.com>
2022-02-21 18:55:55 +0530

    route paths added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import React from 'react'
-import Admin from './components/admin/Admin'
-import User from './components/userPage/User'
-import LoginPage from './components/loginPage/LoginPage'
+import React from "react";
+import Admin from "./components/admin/Admin";
+import User from "./components/userPage/User";
+import LoginPage from "./components/loginPage/LoginPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-    <LoginPage/>
-    <Admin />
-    <User/>
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/Admin" element={<Admin />} />
+        <Route path="/User" element={<User />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
