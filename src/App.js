73b509631c9f7e5fb7f8fab257c25c13bded5137
OMKARBRO <omkarmugutrao1327@gmail.com>
2022-02-21 18:57:13 +0530

    HomePage component created

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import Admin from './components/admin/Admin'
 import User from './components/userPage/User'
 import LoginPage from './components/loginPage/LoginPage'
+import HomePage from './components/homePage/HomePage'
 
 function App() {
   return (
     <div>
     <LoginPage/>
+    <HomePage/>
     <Admin />
     <User/>
     </div>
---
+++ src/components/homePage/HomePage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function HomePage() {
+  return (
+    <div>Hii This HomePage</div>
+  )
+}
+
+export default HomePage
