43069de4e9257e64a34fd650cef326e739085e7f
Priyeshnd555 <priyeshnd555@gmail.com>
2022-02-21 17:21:18 +0530

    loginPage added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import { useState } from "react";
+import { useEffect } from "react";
 
 function App() {
+  
+  // storing user Data in state
+
+  const [userData, setuserData] = useState({
+    name: "",
+    password: "",
+  });
+
+  // print state when validated
+
+  useEffect(() => {
+    console.log("userData from useEffect", userData);
+  }, [userData]);
+
+  //  onSumbit callback function
+
+  const validateUser = () => {
+    console.log("onSubmit", userData);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h5>welcome to our App</h5>
+
+      <input
+        type="text"
+        placeholder="name.."
+        onChange={(e) => {
+          setuserData({ ...userData, name: e.target.value });
+        }}
+      />
+
+      <br></br>
+      <br></br>
+
+      <input
+        type="password"
+        placeholder="pwd.."
+        onChange={(e) =>
+          setuserData({
+            ...userData,
+            password: e.target.value,
+          })
+        }
+      />
+      <br></br>
+      <br></br>
+      <button type="submit" onClick={validateUser}>
+        sumbit
+      </button>
     </div>
   );
 }
